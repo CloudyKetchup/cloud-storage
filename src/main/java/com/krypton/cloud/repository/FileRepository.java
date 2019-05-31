c6c6953568321c4d44dfda8736b78549f06d42fc
krypton17 <maxdodon25@gmail.com>
2019-05-31 13:01:38 -0700

    repositories

---
+++ src/main/java/com/krypton/cloud/repository/FileRepository.java
@@ -0,0 +1,16 @@
+package com.krypton.cloud.repository;
+
+import com.krypton.cloud.model.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
+
+    @Query(value = "select * from file where file.name = :name", nativeQuery = true)
+    Optional<File> getByName(@Param("name") String name);
+}
---
+++ src/main/java/com/krypton/cloud/repository/FolderRepository.java
@@ -0,0 +1,17 @@
+package com.krypton.cloud.repository;
+
+import com.krypton.cloud.model.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+
+    @Query(value = "select * from folder where folder.name = :name", nativeQuery = true)
+    Folder getByName(@Param("name") String name);
+
+    @Query(value = "select * from folder where folder.path = :path", nativeQuery = true)
+    Folder getByPath(@Param("path") String path);
+}
