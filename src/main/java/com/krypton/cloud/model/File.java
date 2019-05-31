6ab3284cc706cab97b3efdb6aee98619631d1d1e
krypton17 <maxdodon25@gmail.com>
2019-05-31 13:00:30 -0700

    file model

---
+++ src/main/java/com/krypton/cloud/model/File.java
@@ -0,0 +1,41 @@
+package com.krypton.cloud.model;
+
+import com.krypton.cloud.model.common.EntityType;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class File {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	@Column
+	private String name;
+
+	@Column
+	private String path;
+
+	@Column
+	private EntityType type = EntityType.FILE;
+
+	@Column
+	private long timeUploaded;
+
+	@Column
+	private long lastTimeAccessed;
+
+	@Column
+	private float size;
+
+	public File() {}
+
+	public File(java.io.File file) {
+		this.path = file.getPath();
+		this.name = file.getName();
+		this.size = (float) file.length() /1024 /1024;
+	}
+}
