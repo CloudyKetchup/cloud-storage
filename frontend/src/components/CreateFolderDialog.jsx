aca04a48e7832f516a8396c7cdd44c0cede37ccd
krypton17 <maxdodon25@gmail.com>
2019-06-07 13:10:08 -0700

    components now is function

--- frontend/src/components/CreateFolderDialog.jsx
+++ frontend/src/components/CreateFolderDialog.jsx
@@ -1,39 +1,35 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class CreateFolderDialog extends Component {
+const CreateFolderDialog = props => (
 
-	render() {
-		return (
-			<div className="create-folder-dialog">
-				<div className="dialog-header">
-					<button
-						className="prev-button"
-						onClick={this.props.closeDialog}
-					>
-						<i className="fas fa-chevron-left"></i>
-					</button>
-					<i className="fas fa-folder"></i>
-					<span>Create new Folder</span>
-				</div>
-				<div 
-					className="container"
-					style={{ textAlign : 'center' }}
-				>
-			        <div className="input-field col s6">
-			          	<input 
-			          		placeholder="Name"
-			          		id="folder-name"
-			          		type="text"
-			          	></input>
-			        </div>
-			        <button 
-			        	className="ok-button"
-			        	onClick={() => this.props.sendFolder(document.getElementById('folder-name').value)}
-			        >OK</button>
-		        </div>
+	<div className="standard-dialog">
+		<div className="dialog-header">
+			<button
+				className="prev-button"
+				onClick={props.closeDialog}
+			>
+				<i className="fas fa-chevron-left"></i>
+			</button>
+			<i className="fas fa-folder"></i>
+			<span>Create new Folder</span>
+		</div>
+		<div 
+			className="container"
+			style={{ textAlign : 'center' }}
+		>
+			<div className="input-field col s6">
+				<input 
+					placeholder="Name"
+					id="folder-name"
+					type="text"
+				></input>
 			</div>
-		);
-	}
-};
+			<button 
+				className="ok-button"
+				onClick={() => props.sendFolder(document.getElementById('folder-name').value)}
+			>OK</button>
+		</div>
+	</div>
+);
 
 export default CreateFolderDialog;
