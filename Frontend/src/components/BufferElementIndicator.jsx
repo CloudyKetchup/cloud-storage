1862f124605f2f14dbb8fbf352aac42139eea742
krypton17 <maxdodon25@gmail.com>
2019-06-20 09:26:06 -0700

    now is placed at bottom right

--- Frontend/src/components/BufferElementIndicator.jsx
+++ Frontend/src/components/BufferElementIndicator.jsx
@@ -4,7 +4,7 @@ const BufferElementIndicator = props => (
     
     <div className="buffer-element">
         <div className="buffer-element-indicator">
-            <i className="fab fa-buffer"/>
+        	<i className="fas fa-paste"/>
         </div>
         <div className="buffer-element-name">
             <span>{props.element.data.name}</span>
