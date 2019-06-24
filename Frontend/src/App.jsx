cf8b7ed8f9dac00b3564547cc3a7aaf411f15b07
krypton17 <maxdodon25@gmail.com>
2019-06-24 09:43:07 -0700

    notification component for right panel

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 
-import Nav 						from './components/Nav'
-import SideBar 					from './components/SideBar';
-import ContentContainer 		from './components/ContentContainer';
-import PrevFolderButton 		from './components/PrevFolderButton';
-import BufferElementIndicator 	from './components/BufferElementIndicator';
-import ElementInfoContainer 	from './components/ElementInfoContainer';
-import DragAndDrop              from './components/DragAndDrop';
-import FileUploadManager         from './components/FileUploadManager';
-import { UploadFile }           from './components/FileUploadManager';
+import NavBar 						from './components/main/navbar/NavBar'
+import LeftPanel 					from './components/main/panel/leftpanel/LeftPanel';
+import ContentContainer 			from './components/content/container/ContentContainer';
+import PrevFolderButton 			from './components/content/container/control/PrevFolderButton';
+import BufferElementIndicator 		from './components/bufferelement/BufferElementIndicator';
+import { ElementInfoContainer } 	from './components/content/elements/info/ElementInfoContainer';
+import DragAndDrop              	from './components/content/dragdrop/DragAndDrop';
+import FileUploadManager        	from './components/content/upload/UploadManager';
+import { UploadFile }           	from './components/content/upload/UploadManager';
+import RightPanel               	from './components/main/panel/rightpanel/RightPanel';
+import CreateFolderDialog       	from './components/content/container/control/CreateFolderDialog';
+import RenameElementDialog 			from './components/content/elements/rename/RenameElementDialog';
+import FolderZippingNotification 	from './components/main/panel/rightpanel/FolderZippingNotification/FolderZippingNotification';
+
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import axios from 'axios';
+import { NotificationType } from './components/main/panel/rightpanel/notification/Notification';
 
 const API_URL 	= 'http://localhost:8080';
 
@@ -21,21 +28,18 @@ export default class App extends Component {
             folders 	            : [],
             files 		            : [],
             bufferElement           : undefined,
-            elementInfoContainer    : false,
             elementSelected         : undefined,
             folderInfo 	            : this.folderInfo,
             rootOpened 	            : true,
             rootMemory	            : {},
-            renameElementDialog     : false,
-            createFolderDialog 	    : false,
-            uploadingFiles          : []
+            uploadingFiles          : [],
+            notifications           : []
         };
 
         window.onkeyup = e => {
             const key = e.keyCode ? e.keyCode : e.which;
 
             if (key === 27) {
-                this.toggleDialogs();
                 this.toggleElementInfo();
             }
         }
@@ -87,22 +91,22 @@ export default class App extends Component {
             .catch(error => console.log(error));
     };
 
-    handleContextMenuAction = (action, element) => {
+    handleContextMenuAction = (action, target) => {
         switch(action) {
+            case 'download':
+                this.sendDownloadRequest(target);
+                break;
             case 'cut':
                 this.setState({bufferElement : {
                         action  : 'cut',
-                        data 	: element
+                        data 	: target
                 }});
                 break;
             case 'copy':
                 this.setState({bufferElement : {
                         action  : 'copy',
-                        data 	: element
+                        data 	: target
                 }});
-                break;
-            case 'rename':
-                this.toggleDialogs(true, false, false);
                 break;
             case 'upload-files':
                 document.getElementById("select-upload-files").click();
@@ -114,31 +118,71 @@ export default class App extends Component {
                 console.log(action);
                 break;
             case 'paste':
-                this.sendPasteAction(element, action);
+                this.sendPasteAction(target, action);
                 break;
             case 'delete':
-                this.sendDeleteRequest(element);
-                break;
-            case 'info':
-                this.toggleElementInfo(true);
+                this.sendDeleteRequest(target);
                 break;
             default: break;
         }
     };
 
-    toggleDialogs(
-        renameElementDialog = false,
-        createFolderDialog 	= false,
-        uploadFileDialog 	= false
-    ) {
-        this.setState({
-            'renameElementDialog' : renameElementDialog,
-            'createFolderDialog'  : createFolderDialog,
-            'uploadFileDialog' 	  : uploadFileDialog
-        });
-    }
+    addNotification = async data => {
+        const notifications = this.state.notifications;
 
-    sendNewFolder = folder =>{
+        notifications.push(data);
+
+        this.setState({ notifications : notifications });
+    };
+
+    sendDownloadRequest = async target => {
+        const targetType = target.type;
+        const targetName = target.name;
+
+        this.addNotification({
+            id 			: target.path,
+            type 		: NotificationType.processing,
+            message 	: `Processing "${target.name}"`,
+            targetType 	: targetType,
+            folderName 	: target.name,
+            processing 	: true
+        });
+
+        axios.request({
+                url: `${API_URL}/${target.type.toLowerCase()}/${target.id}/download`,
+                method: 'GET',
+                responseType: 'blob',
+                onDownloadProgress : p => {
+                    if (((p.total - (p.total - p.loaded)) / p.total * 100) < 100) {
+                        this.updateNotificationProcessing(target);
+                    }
+                }
+            })
+            .then(response => {
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                
+                const link = document.createElement('a');
+                
+                link.href = url;
+                
+                link.setAttribute('download', targetType === 'FILE' ? targetName : `${targetName}.zip`);
+                
+                document.body.appendChild(link);
+                
+                link.click();
+            })
+            .catch(error => console.log(error));
+    };
+
+    updateNotificationProcessing = target => {
+        const notifications = this.state.notifications;
+
+		notifications.filter(__ => __.id === target.path)[0].processing = false;
+
+        this.setState({ notifications : notifications });
+    };
+
+    sendNewFolder = folder => {
         axios.post(`${API_URL}/folder/create/`,
             {
                 'name' 		: folder,
@@ -177,22 +221,22 @@ export default class App extends Component {
         const renameTarget 	= this.state.elementSelected;
         const targetType   	= renameTarget.type.toLowerCase();
 
-        axios.post(`${API_URL}/${targetType}/rename`,{
+        axios.post(`${API_URL}/${targetType}/rename`,
+            {
                 'path' : renameTarget.path,
                 'newName' : newName
             })
             .then(response => {
                 if (response.data === 'OK') {
-                    this.setState({ renameElementDialog : false });
-
-                    this.updateFolderInfo()
+                    this.updateFolderInfo();
                 }
             })
             .catch(error => console.log(error));
     };
 
     sendDeleteRequest = target => {
-        axios.post(`${API_URL}/${target.type.toLowerCase()}/delete`,{
+        axios.post(`${API_URL}/${target.type.toLowerCase()}/delete`,
+            {
                 'path' : target.path
             })
             .then(response => response.data === 'OK'
@@ -210,10 +254,6 @@ export default class App extends Component {
                 ? this.updateFolderInfo()
                 : undefined)
             .catch(error => console.log(error));
-    }
-
-    toggleElementInfo = (toggle = false) => {
-        this.setState({ elementInfoContainer : toggle });
     }
 
     uploadFiles = files => {
@@ -248,41 +288,61 @@ export default class App extends Component {
             .catch(error => console.log(error));
     };
 
+    createFolder() {
+        return <CreateFolderDialog
+                parent={this}
+                sendFolder={folder => this.sendNewFolder(folder)}
+                />;
+    }
+
+    renameDialog() {
+        return this.state.elementSelected !== undefined
+                ? <RenameElementDialog
+                    element={this.state.elementSelected}
+                    onRename={newName => this.sendRenameRequest(newName)}
+                    />
+                : null;
+    }
+
+    elementInfoContainer() {
+        return this.state.elementSelected !== undefined
+                ? <ElementInfoContainer parent={this} data={this.state.elementSelected}/>
+                : null;
+    }
+
     render() {
         return (
-            <main style={{height : '100%'}}>
-                <Nav>
-                    {this.state.bufferElement !== undefined
-                    &&
-                    <BufferElementIndicator element={this.state.bufferElement}/>}
-                </Nav>
-                <SideBar
+            <Router>
+            <div style={{height : '100%'}}>
+                <NavBar/>
+                <LeftPanel
                     memory={this.state.rootMemory}
                     folderInfo={this.state.folderInfo}
                     folders={this.state.folders.length}
                     files={this.state.files.length}
                 />
-                <DragAndDrop className="drag-and-drop" handleDrop={this.uploadFiles}>
-                    <ContentContainer
-                        parent={this}
-                        files={this.state.files}
-                        folders={this.state.folders}
-                    >
+                <DragAndDrop className="drag-and-drop" handleDrop={this.uploadFiles}>                    
+                        <ContentContainer
+                            parent={this}
+                            files={this.state.files}
+                            folders={this.state.folders}
+                        >
+                        <Switch>
+                            <Route path="/rename" component={() => this.renameDialog()}/>
+                            <Route path="/create-folder" component={() => this.createFolder()}/>
+                            <Route path="/element-info" component={() => this.elementInfoContainer()}/>
+                        </Switch>
                         <PrevFolderButton
                             whenClicked={() => this.updateFolderInfo(this.state.folderInfo.parentId)}
                             rootOpened={this.state.rootOpened}
                         />
-                        {this.state.elementSelected !== undefined
-                        &&
-                        this.state.elementInfoContainer
-                        &&
-                        <ElementInfoContainer parent={this} data={this.state.elementSelected}/>}
-                        <button
-                            className='create-folder'
-                            onClick={() => this.toggleDialogs(false, true, false)}
-                        >
-                            <i className='fas fa-folder-plus'/>
-                        </button>
+                        <Link to="/create-folder">
+                            <button
+                                className='create-folder'
+                            >
+                                <i className='fas fa-folder-plus'/>
+                            </button>
+                        </Link>
                         <button
                             className='upload-file-button'
                             onClick={() => document.getElementById('select-upload-files').click()}
@@ -292,10 +352,17 @@ export default class App extends Component {
                         {this.state.uploadingFiles.length > 0
                         &&
                         <FileUploadManager onClose={() => this.setState({ uploadingFiles : [] })}>
-                            {this.state.uploadingFiles.map(file => <UploadFile key={file.name} name={file.name} data={file} parent={this}/>)}
-                        </FileUploadManager>}
+                            {this.state.uploadingFiles.map(file => <UploadFile key={file.name} data={file} parent={this}/>)}
+                        </FileUploadManager>}    
+                        {this.state.bufferElement !== undefined
+                        &&
+                        <BufferElementIndicator element={this.state.bufferElement}/>}
                     </ContentContainer>
                 </DragAndDrop>
+                <RightPanel closePanel={() => document.getElementById("right-panel").style.right = '-300px'}>
+                	{this.state.notifications.map(notification => 
+                		<FolderZippingNotification key={notification.path} folderName={notification.folderName} processing={notification.processing}/>)}
+                </RightPanel>
                 <input
                     id="select-upload-files"
                     type="file"
@@ -306,7 +373,8 @@ export default class App extends Component {
                     }}
                     style={{ display : 'none' }}
                     multiple/>
-            </main>
+            </div>
+            </Router>
         );
     }
 }
