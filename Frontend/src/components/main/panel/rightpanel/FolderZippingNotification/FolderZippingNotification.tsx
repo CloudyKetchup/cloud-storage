255b89817dfd63c2f1a9d318ee350962f61bda08
krypton17 <maxdodon25@gmail.com>
2019-06-24 22:35:57 -0700

    moved styles to index.css

--- Frontend/src/components/main/panel/rightpanel/FolderZippingNotification/FolderZippingNotification.tsx
+++ Frontend/src/components/main/panel/rightpanel/FolderZippingNotification/FolderZippingNotification.tsx
@@ -1,45 +1,28 @@
 import React from 'react';
 
-import {Notification, NotificationProps}    from "../notification/Notification";
-import CircularProgress                     from '@material-ui/core/CircularProgress';
+import {Notification}       from '../notification/Notification';
+import CircularProgress     from '@material-ui/core/CircularProgress';
 
-interface FolderZippingNotificationProps extends NotificationProps {
+type FolderZippingNotificationProps = {
     folderName: string
     processing: boolean
 }
 
 export default class FolderZippingNotification extends Notification<FolderZippingNotificationProps> {
+    
     render() {
         return (
             <div className="notification">
                 {this.icon(<i className="fas fa-file-archive"/>)}
-                <div style={{
-                    display: 'flex',
-	                float: 'right',
-	                width: 'calc(-90px + 100%)',
-	                height: 'calc(-20px + 100%)',
-	                padding: '10px'
-                }}>
+                <div className="notification-content">
                     {this.message(`Processing "${this.props.folderName}"`)}
                     <div style={{
 	                    height: '40px',
 	                    float: 'right'
                     }}>
                         {this.props.processing
-                            ?   <CircularProgress size={20} style={{
-                                    marginTop: '5px',
-                                    marginRight : '5px',
-	                                width: '30px',
-	                                float: 'right',
-                                    height: '30px',
-                                    color: '#ff723a'
-                                }}/>
-                            :   <div style={{
-                                    background: 'transparent',
-                                    color: 'rgb(43, 206, 54)',
-                                    fontSize: '20px',
-                                    height: '100%'
-                                }}>
+                            ?   <CircularProgress className="circular-progress" size={30} style={{ color: '#ff723a' }}/>
+                            :   <div className="check-button">
                                     <i className="fas fa-check" style={{ lineHeight : '45px', marginRight : '10px' }}/>
                                 </div>}
                     </div>
