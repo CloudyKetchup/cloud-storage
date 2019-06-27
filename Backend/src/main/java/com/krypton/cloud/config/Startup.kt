b870ce09187c7b12b7e08c3e7398baf93c923305
krypton17 <maxdodon25@gmail.com>
2019-06-27 10:16:02 -0700

    code clean up

--- Backend/src/main/java/com/krypton/cloud/config/Startup.kt
+++ Backend/src/main/java/com/krypton/cloud/config/Startup.kt
@@ -1,35 +1,33 @@
 package com.krypton.cloud.config
 
 import com.krypton.cloud.service.file.record.FileRecordServiceImpl
-import com.krypton.cloud.service.folder.record.FolderPersistenceHelper
 import com.krypton.cloud.service.folder.record.FolderRecordServiceImpl
 import com.krypton.cloud.service.folder.record.FolderRecordUtils
 import lombok.AllArgsConstructor
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Service
-
 import java.io.File
-import java.util.Arrays
+import java.util.*
 
 @Service
 @AllArgsConstructor
 class Startup(
-        private val fileRecordService: FileRecordServiceImpl,
-        private val folderRecordService: FolderRecordServiceImpl,
-        private val folderRecordUtils: FolderRecordUtils
+        private val fileRecordService   : FileRecordServiceImpl,
+        private val folderRecordService : FolderRecordServiceImpl,
+        private val folderRecordUtils   : FolderRecordUtils
 ) : CommandLineRunner {
+
+    private val root = File("C:\\Users\\dodon\\cloud")
 
     override fun run(vararg args: String) {
 
-        val rootFolder = File("C:\\Users\\dodon\\cloud")
-
-        if (!rootFolder.exists()) {
-            rootFolder.mkdir()
+        if (!root.exists()) {
+            root.mkdir()
         }
 
-        val rootContent = Arrays.asList(*rootFolder.listFiles()!!)
+        val rootContent = Arrays.asList(*root.listFiles())
 
-        folderRecordService.save(rootFolder)
+        folderRecordService.save(root)
 
         folderRecordUtils.addAllFoldersToDatabase(rootContent)
 
