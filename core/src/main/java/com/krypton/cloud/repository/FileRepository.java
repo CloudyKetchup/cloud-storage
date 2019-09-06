0596e396fd67b6a6e8472e97928fc12243ff752b
Krypton17 <maxdodon25@gmail.com>
2019-09-06 18:44:59 +0300

    added getWithOptional that return File entity wrapped with Optional

--- core/src/main/java/com/krypton/cloud/repository/FileRepository.java
+++ core/src/main/java/com/krypton/cloud/repository/FileRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
+
+    @Query(value = "select * from file where file.path = :path", nativeQuery = true)
+    Optional<File> getWithOptional(@Param("path") String path);
 
     @Query(value = "select * from file where file.path = :path", nativeQuery = true)
     File getByPath(@Param("path") String path);
