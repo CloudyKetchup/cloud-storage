0d604de30821a4f2cd56769699f269ec41994c9a
Krypton17 <maxdodon25@gmail.com>
2019-09-18 08:53:46 +0300

    id is now a string because uses UUID

--- Frontend/src/model/NavNode.ts
+++ Frontend/src/model/NavNode.ts
@@ -1,12 +1,12 @@
 export default class NavNode {
 	
-	id: number;
+	id: string;
 	name: string;
 	onClick: () => void;
 	prev: NavNode | undefined;
 	next: NavNode | null = null;
 
-	constructor(id: number, name: string, prev: NavNode | undefined, onClick: () => void) {
+    constructor(id: string, name: string, prev: NavNode | undefined, onClick: () => void) {
 		this.id 	= id;
 		this.name	= name;
 		this.onClick	= onClick;
