a6d11ea0c53c4382163537d0a22ea615330c851d
Krypton17 <maxdodon25@gmail.com>
2019-09-30 12:38:13 +0300

    nullability check

--- util/src/main/kotlin/util/folder/FolderTools.kt
+++ util/src/main/kotlin/util/folder/FolderTools.kt
@@ -7,11 +7,13 @@ object FolderTools {
     fun getFolderLength(folder : File) : Long {
         val length = LongArray(1)
 
-        for (child in folder.listFiles()!!) {
-            if (child.isFile)
-                length[0] += child.length()
-            else if (child.isDirectory)
-                length[0] += getFolderLength(child)
+        if (folder.listFiles() != null) {
+            for (child in folder.listFiles()!!) {
+                if (child.isFile)
+                    length[0] += child.length()
+                else if (child.isDirectory)
+                    length[0] += getFolderLength(child)
+            }
         }
         return length[0]
     }
