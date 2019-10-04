3069c70ee5a40e27a119ec765b1620dc2c678d16
Krypton17 <maxdodon25@gmail.com>
2019-10-04 15:15:01 +0300

    extended ContentContext from BasicContentContext

--- web-app/src/context/ContentContext.tsx
+++ web-app/src/context/ContentContext.tsx
@@ -1,14 +1,15 @@
-import { Component, createContext } from "react";
 import { FileEntity } from "../model/entity/FileEntity";
 import { FolderEntity } from "../model/entity/FolderEntity";
 import { Entity } from "../model/entity/Entity";
 
-export type ContentContextType = {
+export type BasicContentContext = {
     files : FileEntity[]
     folders : FolderEntity[]
-    trashItems : Entity[]
-    setFiles : (newFiles : FileEntity[]) => FileEntity[]
+    setFiles: (newFiles: FileEntity[]) => FileEntity[]
     setFolders : (newFolders : FolderEntity[]) => FolderEntity[]
-    setTrashItems : (newTrashItems : Entity[]) => Entity[]
 };
 
+export interface ContentContextInterface extends BasicContentContext {
+    trashItems : Entity[]
+    setTrashItems : (newTrashItems : Entity[]) => Entity[]
+};
