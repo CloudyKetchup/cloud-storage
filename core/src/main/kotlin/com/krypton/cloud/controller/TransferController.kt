346cb52c01bc69faebbacdbe82697ea468cbf464
Krypton17 <maxdodon25@gmail.com>
2019-11-03 02:57:59 +0200

    get mapping for getting image

--- core/src/main/kotlin/com/krypton/cloud/controller/TransferController.kt
+++ core/src/main/kotlin/com/krypton/cloud/controller/TransferController.kt
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor
 import org.springframework.core.io.InputStreamResource
 
 import org.springframework.core.io.Resource
+import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -16,38 +17,45 @@ import java.util.*
 @CrossOrigin
 @RestController
 @AllArgsConstructor
-class TransferController(
-	private val fileRecordServiceImpl: FileRecordServiceImpl
-) {
+class TransferController(private val fileRecordServiceImpl: FileRecordServiceImpl) {
 
 	/**
 	 * Download file from input stream
 	 *
 	 * @param path
 	 * */
-	 @GetMapping("/file/{path}/download")
-	 fun downloadFile(@PathVariable path : String) : ResponseEntity<Resource> {
-		 val file = File(path)
-		 val inputStream = file.inputStream()
+	@GetMapping("/file/{path}/download")
+	fun downloadFile(@PathVariable path : String) : ResponseEntity<Resource> {
+		val file = File(path)
+		val inputStream = file.inputStream()
 
-		 return ResponseEntity
-				.ok()
-				.contentType(MediaType.parseMediaType("application/octet-stream"))
-				.headers(HttpHeaders().apply { set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=${file.name}") })
-				.body(InputStreamResource(inputStream))
-	 }
+		return ResponseEntity
+			.ok()
+			.contentType(MediaType.parseMediaType("application/octet-stream"))
+			.headers(HttpHeaders().apply { set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=${file.name}") })
+			.body(InputStreamResource(inputStream))
+	}
 
-	 @GetMapping("/file/{id}/image")
-	 fun getImage(@PathVariable id : String) : ResponseEntity<ByteArray> {
-		 val file = fileRecordServiceImpl.getById(UUID.fromString(id))
+	@GetMapping("/file/{id}/thumbnail")
+	fun getThumbnail(@PathVariable id : String) : ResponseEntity<ByteArray> {
+		val file = fileRecordServiceImpl.getById(UUID.fromString(id))
 
-		 if (file != null && file.image != null) {
-			 val thumbnail = MediaService.getThumbnail(file.image.thumbnailPath)
+		if (file != null && file.image != null) {
+			val thumbnail = MediaService.getThumbnail(file.image.thumbnailPath)
 
-			 if (thumbnail != null && thumbnail.body != null) {
+			if (thumbnail != null && thumbnail.body != null) {
 				return thumbnail
-			 }
-		 }
-		 return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(File(file!!.path).readBytes())
-	 }
- }
+			}
+		}
+		return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(File(file!!.path).readBytes())
+	}
+
+	@GetMapping("/file/{id}/image")
+	fun getImage(@PathVariable id : String) : HttpEntity<*> {
+		val file = fileRecordServiceImpl.getById(UUID.fromString(id))
+
+		return if (file != null && file.image != null) {
+			ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(File(file.path).readBytes())
+		} else ResponseEntity.EMPTY
+	}
+}
